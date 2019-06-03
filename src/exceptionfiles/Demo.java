7a385c154600b32054881ed9320fee1539b984b2
Andreas <andreas.sosilo@gmail.com>
2019-06-03 18:53:56 +0700

    Add exceptionfiles package

---
+++ src/exceptionfiles/Demo.java
@@ -0,0 +1,19 @@
+package exceptionfiles;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("Program starts");
+		doMath(5, 0);
+		System.out.println("Program continues");
+	}
+	
+	public static void doMath(int a, int b) {
+		answer(a,b);
+	}
+	
+	public static double answer(int x, int y) {
+		// Catch error HERE!
+		return x/y;
+	}
+}
